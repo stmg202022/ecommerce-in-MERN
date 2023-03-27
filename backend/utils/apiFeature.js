ec13f000d7005e363d2ae526246545df9ac101b8
Samson Tmg <stmg202023@gmail.com>
2023-03-27 22:50:52 +0545

    added class for search(), filter() and pagination() for products

---
+++ backend/utils/apiFeature.js
@@ -0,0 +1,79 @@
+class ApiFeatures {
+    constructor(query, queryStr){
+        this.query = query,
+        this.queryStr = queryStr
+    }
+
+    search(){
+        const keyword = this.queryStr.keyword 
+        ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: "i" //case sensitive
+            }
+        }: {};
+
+        console.log(keyword); // this is obj created to find name if keyword exist
+
+        this.query = this.query.find({...keyword})
+        return this;
+
+    }
+
+
+    filter(){
+        const copyQuery = {...this.queryStr}
+
+        console.log(copyQuery);
+
+
+        //filter for categories
+        const removeQuery = ["keyword", "page", "limit"] //categories not remove by this filter fn when categories send
+
+        removeQuery.forEach(key => delete copyQuery[key])
+
+        // this.query = this.query.find({...copyQuery});
+
+        // console.log(copyQuery)
+
+
+
+        //filter for price
+        let queryStr = JSON.stringify(copyQuery)
+
+        // console.log(queryStr)
+
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`)
+
+        // console.log(queryStr)
+
+        queryStr = JSON.parse(queryStr)
+        
+        this.query = this.query.find(queryStr);
+
+        console.log(queryStr)
+
+        return this;
+    }
+
+
+    pagination(resultPerPage){
+
+        console.log(typeof this.queryStr.page);
+        const currentPage = Number(this.queryStr.page) || 1;
+
+        console.log( currentPage);
+
+        let skip = resultPerPage * ( currentPage - 1); // 0, 5, 10, 15
+
+        this.query = this.query.limit(resultPerPage).skip(skip);  //0-5, 5-10, 10-15, 15-20 -> 5 per/page
+
+        return this
+
+
+    }
+
+}
+
+
+module.exports = ApiFeatures;
