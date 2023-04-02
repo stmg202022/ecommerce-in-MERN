f8a9d92a7671c0222c710251f0fe745fb1d040f7
Samson Tmg <stmg202023@gmail.com>
2023-04-02 08:10:10 +0545

    added user router

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const cookiesParser = require("cookie-parser");
 const app = express();
-// const errorMiddleware = require("./middleware/error")
-app.use(express.json())
+app.use(cookiesParser());
 
-//routes Import 
-const productRouter = require("./routes/productRoute")
+const errorMiddleware = require("./middleware/error");
+app.use(express.json());
 
+//routes Import
+const productRouter = require("./routes/productRoute");
+const userRouter = require("./routes/userRouter");
 //app connecting with routes
-app.use("/api/v1", productRouter.router)
+app.use("/api/v1", productRouter.router);
+app.use("/api/v1", userRouter.router);
 
 //middleWare for Errors
-// app.use(errorMiddleware);
-
+app.use(errorMiddleware);
 
 module.exports = app;
