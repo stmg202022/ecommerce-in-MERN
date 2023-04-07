6d03ae050019730d9565935ec4a80d8ef1c60b27
Samson Tmg <stmg202023@gmail.com>
2023-04-07 06:19:11 +0545

    added user resetPassword and assign new password and save it using tokenand email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,13 @@
 const User = require("../model/userModel");
 const ErrorHandler = require("../utils/errorHandler");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail.js");
+const crypto = require("crypto");
 
 exports.registerUser = async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  const user = await new User({
+  const user = new User({
     name,
     email,
     password,
@@ -55,6 +57,7 @@ exports.userLogin = async (req, res, next) => {
   if (!user) {
     return await next(new ErrorHandler("Invalid Email or Password.", 401));
   }
+
   //THIS comparePassword is userSchema.methods.comparePassword
   const isPasswordMatch = await user.comparePassword(password);
 
@@ -83,4 +86,91 @@ exports.logOut = async (req, res, next) => {
     success: true,
     message: "LOGOUT SUCCESS",
   });
+};
+
+//Forgot Password
+exports.forgotPassword = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User Not Found", 404));
+  }
+
+  //if user found then we need resetToken
+  const resetToken = await user.getResetpasswordToken(); //await
+
+  //==============================================================
+  // console.log(resetToken);
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = ` Your password reset token is :- \n\n ${resetPasswordUrl} \n\nif you have not requested this email then please ignore it. `;
+
+  //==================================================================
+  // console.log(resetPasswordUrl);
+
+  try {
+    await sendEmail({
+      //to
+      email: user.email,
+      subject: `CompanyName Password Recovery.`,
+      message,
+      // message: message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email send to ${user.email} Successfully.`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+};
+
+//Reset Password
+exports.resetPassword = async (req, res, next) => {
+  //
+
+  console.log(req.params.token);
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token) // getting back to resetToken
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  // console.log(user);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "RESET PASSWORD TOKEN IS INVALID OR HAS BEEN EXPIRED.",
+        404
+      )
+    );
+  } else if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("PASSWORD DOES NOT MATCH", 400));
+  } else {
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    user.save().then((result) => {
+      sendToken(result, 200, res);
+    });
+  }
+
+  // console.log(user);
 };
