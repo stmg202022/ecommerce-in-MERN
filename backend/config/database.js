f826b007285e103e98dc3c13775256a347008bf2
Samson Tmg <stmg202023@gmail.com>
2023-04-07 06:14:27 +0545

    just checked the connections of data

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDataBase = async () => {
+  try {
+    const res = await mongoose.connect(process.env.DB_URL);
 
-    try {
-        const res = await mongoose.connect(process.env.DB_URL);
+    const data = res.connection.host;
 
-        const data =  res.connection.host;
-        
-        console.log(`DATABASE CONNECTED WITH SERVER ${data}`)
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
+    console.log(`DATABASE CONNECTED WITH SERVER ${data}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = connectDataBase;
