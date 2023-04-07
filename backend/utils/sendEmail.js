f4820676d53eebb951aecd8effe639f52ec53e7d
Samson Tmg <stmg202023@gmail.com>
2023-04-07 06:28:34 +0545

    added semdMail for sending link to get token for user using nodeMailer through email message

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodeMailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  //CREATE TRANSPORTER
+  const transporter = nodeMailer.createTransport({
+    // host: "smtp.gmail.com",
+    // port: 465,
+    service: process.env.SMPT_SERVICE, //simple mail tramsfer protocal
+    auth: {
+      user: process.env.SMPT_EMAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  //CREATE MAILOPTIONS
+  const mailOptions = {
+    from: process.env.SMPT_EMAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
