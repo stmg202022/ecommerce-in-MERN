67e9a0c084f391a3401226183f51d5714fcf04b6
Samson Tmg <stmg202023@gmail.com>
2023-04-07 23:12:38 +0545

    added CRUD for user by admin router

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -9,6 +9,9 @@ const {
   userChangePassword,
   userChangeProfile,
   getAllUsers,
+  getSingleUser,
+  updateUserRole,
+  adminDeleteUser,
 } = require("../controllers/userController");
 const router = express.Router();
 const { isAuthenticatedUser, authorizRoles } = require("../middleware/auth");
@@ -21,6 +24,29 @@ router.put("/password/reset/:token", resetPassword);
 router.get("/me", isAuthenticatedUser, getUserDetails);
 router.put("/password/update", isAuthenticatedUser, userChangePassword);
 router.put("/me/update", isAuthenticatedUser, userChangeProfile);
-router.get("/users", isAuthenticatedUser, authorizRoles("admin"), getAllUsers);
+router.get(
+  "/admin/users",
+  isAuthenticatedUser,
+  authorizRoles("admin"),
+  getAllUsers
+);
+router.get(
+  "/admin/user/:id",
+  isAuthenticatedUser,
+  authorizRoles("admin"),
+  getSingleUser
+);
+router.put(
+  "/admin/update/userrole/:id",
+  isAuthenticatedUser,
+  authorizRoles("admin"),
+  updateUserRole
+);
+router.delete(
+  "/admin/delete/user/:id",
+  isAuthenticatedUser,
+  authorizRoles("admin"),
+  adminDeleteUser
+);
 
 exports.router = router;
