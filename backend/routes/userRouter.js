791e3f57a7ad70eaddf2c53cd60a6e0406ad258b
Samson Tmg <stmg202023@gmail.com>
2023-04-07 06:26:13 +0545

    added user password/forgot and password/reset/:token router

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,11 +3,15 @@ const {
   registerUser,
   userLogin,
   logOut,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", registerUser); //create
 router.post("/login", userLogin);
 router.post("/logout", logOut);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
 
 exports.router = router;
